bd1b98424f53dcd4d60de90c8ddd6d1e28be92b3
ajuste <juste.alvaro@gmail.com>
2015-11-01 17:42:47 -0300

    jaune-engine as specific package

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
-global._require = function(){
-  return function(name) {
-    return require(name.indexOf("/") !== -1 ? [__dirname, name].join("/") : name);
-  };
-};
+"use strict";
 
-require("./lib/utils/boolean");
-require("./lib/utils/array");
-require("./lib/utils/date");
-require("./lib/utils/time");
+// 3rd
+const _extend      = require("lodash").extend;
 
-var Environment = null;
-var Instance    = null;
+// jaune
+const _environment = require("jaune-env");
+const _fs          = require("jaune-fs");
+
+// lib
+const _daemon      = require("./lib/daemon");
+const _db          = require("./lib/db");
+const _errors      = require("./lib/error");
+const _logging     = require("./lib/logging");
+const _mail        = require("./lib/mail");
+const _crypto      = require("./lib/crypto");
+const _security    = require("./lib/security");
 
 module.exports = {
-  init : function(config) {
+  /**
+   * @function Create a new engine based on configuration.
+   * @param    {Object} config The configuration
+   * @returns  {Object} engine
+   */
+  create : function(config) {
 
-    Environment                     = require("./lib/server/environment").init(config);
-    Instance                        = {};
-    Instance.Environment            = Environment;
-    Instance.Reflection             = require("./lib/utils/reflection").Reflection;
-    Instance.UUID                   = require("./lib/utils/uuid").UUID;
-    Instance.Stream                 = {};
-    Instance.Stream.WriteReadStream = require("./lib/utils/streams").WriteReadStream;
-    Instance.Convert                = require("./lib/utils/convert");
-    Instance.Debug                  = require("./lib/utils/debug");
-    Instance.Validator              = require("./lib/utils/validator").Validator;
-    Instance.Crypto                 = {};
-    Instance.Crypto.Hashing         = require("./lib/crypto/hashing").Hashing;
-    Instance.Logging                = {};
-    Instance.Logging.Manager        = require("./lib/logging/logging-manager").LoggingManager;
-    Instance.Error                  = {};
-    Instance.Error.Errors           = require("./lib/error/errors");
-    Instance.Error.Manager          = require("./lib/error/error-manager").Manager;
-    Instance.Security               = require("./lib/security/security");
-    Instance.FileSystem             = require("./lib/filesystem/filesystem-manager");
-    Instance.Daemon                 = {};
-    Instance.Daemon.Status          = require("./lib/daemon/daemon-def").DaemonStatus;
-    Instance.Daemon.Manager         = require("./lib/daemon/daemon-manager").DaemonManager;
-    Instance.Daemon.Daemon          = require("./lib/daemon/daemon").Daemon;
-    Instance.Http                   = {};
-    Instance.Http.Util              = require("./lib/http/http-util").Util;
-    Instance.Http.HttpCode          = require("./lib/http/http-util").HttpCode;
-    Instance.Mail                   = {};
-    Instance.Mail.Manager           = require("./lib/mail/email-manager").Manager;
-    Instance.Db                     = {};
-    Instance.Db.SqlQueryExecutor    = require("./lib/db/db-sql-query-executor").SqlQueryExecutor;
-    Instance.Db.Manager             = require("./lib/db/db-manager").DatabaseManager;
-    Instance.Db.SqlUtil             = require("./lib/db/db-sql-util").SqlUtil;
-    Instance.Locale                 = {};
-    Instance.Locale.Manager         = require("./lib/localization/locale").LocaleManager;
-    Instance.App                    = require("./lib/server/app");
+    const _env      = new _environment(config);
+    const _instance = {};
 
-    return Instance;
-  },
-  get : function() {
-    return Instance;
+    // env namespace
+    _extend(_instance, { Environment : _env });
+
+    // crypto namespace
+    _extend(_instance, { Crypto : _crypto });
+    _extend(_instance, { Crypto : { Hashing : new _crypto.Hashing() }});
+
+    // fs namespace
+    _extend(_instance, { Fs : _fs });
+    _extend(_instance, { Fs : { Manager : new _fs.Manager(_env) }});
+
+    // logging namespace
+    _extend(_instance, { Logging : { Manager : new _logging.Manager(_env) } });
+
+    // error namespace
+    _extend(_instance, { Error : _errors });
+    _extend(_instance, { Error : { Manager : new _errors.Manager() }});
+
+    // security namespace
+    _extend(_instance, { Security : _security });
+    _extend(_instance, { Security : { Manager : new _security.Manager() }});
+
+    // daemon namespace
+    _extend(_instance, { Daemon : _daemon });
+    _extend(_instance, { Daemon : { Manager : new _daemon.Manager() }});
+
+    // mail namespace
+    _extend(_instance, { Mail : _mail });
+    _extend(_instance, { Mail : { Manager : new _mail.Manager(_env) }});
+
+    // db namespace
+    _extend(_instance, { Mail : _mail });
+    _extend(_instance, { Mail : { Manager : new _mail.Manager() }});
+
+
+    _instance.Db                  = {};
+    _instance.Db.SqlQueryExecutor = require("./lib/db/db-sql-query-executor").SqlQueryExecutor;
+    _instance.Db.Manager          = require("./lib/db/db-manager").DatabaseManager;
+    _instance.Db.SqlUtil          = require("./lib/db/db-sql-util").SqlUtil;
+
+    return _instance;
   }
 };
